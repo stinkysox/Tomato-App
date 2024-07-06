fae21be6cf51845cd152a2d7b7363f06ab615777
Hevit <kopelahevit5@gmail.com>
2024-07-06 11:43:11 +0530

    update

--- fontend/src/Context/StoreContext.jsx
+++ fontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [food_list, setFoodList] = useState([]);
 
-  const url = "http://localhost:4000/";
+  const url = "https://tomato-app-backend-s0qz.onrender.com/";
   const [token, setToken] = useState("");
 
   const addToCart = async (itemId) => {
